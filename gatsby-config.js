6153a55b9ad024177628fa77f778967d61148c49
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-12-05 10:48:09 -0600

    contentful setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,28 @@
 module.exports = {
   siteMetadata: {
     title: `Overwhlmed_Gamer`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "M65cMHVAwBLwMRzHA9NgiyI35OO8dHUi5_S1q1PeiAk",
-      "spaceId": "5d4sk1hmrihb"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "asqh3IHo_BQlRUtj0uJ8YWjhOgogJwoPybWE4jbxEsU",
+        spaceId: "5d4sk1hmrihb",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
