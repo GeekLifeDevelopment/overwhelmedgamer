99c58075f15d9df778fe7d9dcd613037afe18091
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-12-13 10:00:40 -0600

    fixing style sheet for build

--- src/pages/index.styled.js
+++ src/pages/index.styled.js
@@ -81,66 +81,6 @@ const HomeStyles = styled.div`
       }
     }
   }
-
-  /* &__description {
-    position: absolute;
-    max-width: 400px;
-    top: 23rem;
-    left: 4rem;
-    padding: 2rem;
-    color: white;
-    border: 2px solid ${(props) => props.theme.colors.owOrange};
-    font-size: 1rem;
-
-    @media (max-width: 950px) {
-      top: 36rem;
-    }
-  } */
-
-  /* .logo-container {
-      position: absolute;
-      width: 35%;
-      right: 10rem;
-      top: 10rem;
-      @media (max-width: 1120px) {
-        width: 30%;
-      }
-      @media (max-width: ${(props) => props.theme.breakpoints.tablet}) {
-        width: 40%;
-        position: relative;
-        margin: auto;
-      }
-
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
-    }
-    .sub-buttons {
-      display: flex;
-      gap: 1rem;
-      position: absolute;
-      bottom: 2rem;
-      left: 4rem;
-    }
-
-    .social-icons {
-      position: absolute;
-      bottom: 2rem;
-      right: 28rem;
-      font-size: 1.3rem;
-
-      a {
-        color: white;
-        padding: 0 1rem;
-
-        &:hover {
-          color: ${(props) => props.theme.colors.owOrange};
-        }
-      }
-    }
-  } */
 `;
 
 export default HomeStyles;
