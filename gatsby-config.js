93463f83e37be085fb795bcc57c309476216ee62
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-12-22 20:14:51 -0600

    removed unnecassary dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,16 +10,16 @@ module.exports = {
     image: `/web-logo.png`,
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "asqh3IHo_BQlRUtj0uJ8YWjhOgogJwoPybWE4jbxEsU",
-        spaceId: "5d4sk1hmrihb",
-      },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    // {
+    //   resolve: "gatsby-source-contentful",
+    //   options: {
+    //     accessToken: "asqh3IHo_BQlRUtj0uJ8YWjhOgogJwoPybWE4jbxEsU",
+    //     spaceId: "5d4sk1hmrihb",
+    //   },
+    // },
+    // "gatsby-plugin-image",
+    // "gatsby-plugin-sharp",
+    // "gatsby-transformer-sharp",
     "gatsby-plugin-styled-components",
     "gatsby-plugin-sitemap",
     {
