c64a7cc4eb2bf587549fdc1904b570bc47729aea
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-12-28 22:18:43 -0600

    added podcast component and episode page

---
+++ src/components/podcasts/Podcasts.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { parseString } from "xml2js";
+import Styles from "./Podcasts.styled";
+import { stripHtmlTags } from "../../helpers";
+
+const Podcasts = () => {
+  const [podcastData, setPodcastData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://feed.podbean.com/jasonft/feed.xml"
+        );
+        const data = await response.text();
+        parseXML(data);
+      } catch (error) {
+        console.error("Error fetching XML:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const parseXML = (xmlData) => {
+    parseString(xmlData, (err, result) => {
+      if (err) {
+        console.error("Error parsing XML:", err);
+      } else {
+        // Process the parsed result (result) here
+        console.log("Parsed XML:", result);
+        setPodcastData(result);
+      }
+    });
+  };
+  {
+    console.log(podcastData);
+  }
+  return (
+    <Styles>
+      <div>
+        {podcastData &&
+          podcastData.rss.channel.map((podcast, index) =>
+            podcast.item.map((episode, episodeIndex) => (
+              <div key={episodeIndex}>
+                <h3 className='episode-title'>{episode.title}</h3>
+                <p>{stripHtmlTags(episode.description.toString())}</p>
+                <audio controls>
+                  <source src={episode.enclosure[0].$.url} type='audio/mpeg' />
+                  Your browser does not support the audio tag.
+                </audio>
+              </div>
+            ))
+          )}
+      </div>
+    </Styles>
+  );
+};
+
+export default Podcasts;
---
+++ src/components/podcasts/Podcasts.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const PodcastStyles = styled.div`
+  border: 2px solid black;
+  margin: 1rem;
+  padding: 1rem;
+
+  .episode-title {
+    font-family: "Kanit", sans-serif;
+  }
+`;
+
+export default PodcastStyles;
