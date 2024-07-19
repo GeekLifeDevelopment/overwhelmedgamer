cfee837654229378a52400984f1d271a9362ae97
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2024-07-19 14:30:45 -0500

    added react helmet and metracool script

--- src/Layout.js
+++ src/Layout.js
@@ -4,10 +4,27 @@ import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import MainNav from "./components/mainNav/MainNav";
 import Footer from "./components/footer/Footer";
+import { Helmet } from "react";
 
 const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        {`
+          function loadScript(a) {
+            var b = document.getElementsByTagName("head")[0],
+              c = document.createElement("script");
+            c.type = "text/javascript";
+            c.src = "https://tracker.metricool.com/resources/be.js";
+            c.onreadystatechange = a;
+            c.onload = a;
+            b.appendChild(c);
+          }
+          loadScript(function() {
+            beTracker.t({ hash: "5072ba0c61061951d87eb34665ca5e4e" });
+          });
+        `}
+      </Helmet>
       <GlobalStyles />
       <MainNav />
       <main style={{ minHeight: "100vh" }}>{children}</main>
