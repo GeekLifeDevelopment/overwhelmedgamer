3b554a68dfc3ca1b4a2d9f222aaaf578b1242f48
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2024-07-19 14:52:15 -0500

    adding tracking

--- src/Layout.js
+++ src/Layout.js
@@ -11,18 +11,7 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={theme}>
       <Helmet>
         {`
-          function loadScript(a) {
-            var b = document.getElementsByTagName("head")[0],
-              c = document.createElement("script");
-            c.type = "text/javascript";
-            c.src = "https://tracker.metricool.com/resources/be.js";
-            c.onreadystatechange = a;
-            c.onload = a;
-            b.appendChild(c);
-          }
-          loadScript(function() {
-            beTracker.t({ hash: "5072ba0c61061951d87eb34665ca5e4e" });
-          });
+          function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"5072ba0c61061951d87eb34665ca5e4e"})});
         `}
       </Helmet>
       <GlobalStyles />
