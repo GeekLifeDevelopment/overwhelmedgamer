860cbea843c5a2b3f78d7e72d7c2293b3be21d63
Jason Sorenson <jasonsorenson@Jasons-MBP.wi.rr.com>
2025-05-30 09:26:41 -0500

    added google tag manager to my site

--- src/html.js
+++ src/html.js
@@ -30,8 +30,33 @@ export default function HTML(props) {
           href='https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500;700&family=Kanit:wght@200;400;500&display=swap'
           rel='stylesheet'
         />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WVGQC95L');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body {...props.bodyAttributes}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVGQC95L"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="gtm"
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {props.preBodyComponents}
         <div
           key={`body`}
